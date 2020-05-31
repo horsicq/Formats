80645db4f754c5a28ccee173e68fca99bbb32e68
horsicq <horsicq@gmail.com>
2020-06-01 01:28:20 +0200

    Fix

--- xpe_def.h
+++ xpe_def.h
@@ -508,33 +508,27 @@ enum ReplacesCorHdrNumericDefines
     COMIMAGE_FLAGS_STRONGNAMESIGNED     =0x00000008,
     COMIMAGE_FLAGS_NATIVE_ENTRYPOINT    =0x00000010,
     COMIMAGE_FLAGS_TRACKDEBUGDATA       =0x00010000,
-
     // Version flags for image.
     COR_VERSION_MAJOR_V2                =2,
     COR_VERSION_MAJOR                   =COR_VERSION_MAJOR_V2,
     COR_VERSION_MINOR                   =5,
     COR_DELETED_NAME_LENGTH             =8,
     COR_VTABLEGAP_NAME_LENGTH           =8,
-
     // Maximum size of a NativeType descriptor.
     NATIVE_TYPE_MAX_CB                  =1,
     COR_ILMETHOD_SECT_SMALL_MAX_DATASIZE=0xFF,
-
     // #defines for the MIH FLAGS
     IMAGE_COR_MIH_METHODRVA             =0x01,
     IMAGE_COR_MIH_EHRVA                 =0x02,
     IMAGE_COR_MIH_BASICBLOCK            =0x08,
-
     // V-table constants
     COR_VTABLE_32BIT                    =0x01,          // V-table slots are 32-bits in size.
     COR_VTABLE_64BIT                    =0x02,          // V-table slots are 64-bits in size.
     COR_VTABLE_FROM_UNMANAGED           =0x04,          // If set, transition from unmanaged.
     COR_VTABLE_FROM_UNMANAGED_RETAIN_APPDOMAIN  =0x08,  // If set, transition from unmanaged with keeping the current appdomain.
     COR_VTABLE_CALL_MOST_DERIVED        =0x10,          // Call most derived method described by
-
     // EATJ constants
     IMAGE_COR_EATJ_THUNK_SIZE           =32,            // Size of a jump thunk reserved range.
-
     // Max name lengths
     //@todo: Change to unlimited name lengths.
     MAX_CLASS_NAME                      =1024,
