bbd1e221ca38ebb0aadc2a394630baef922d29ae
horsicq <horsicq@gmail.com>
2020-07-31 18:17:53 +0200

    Fix

--- xbinary.cpp
+++ xbinary.cpp
@@ -291,16 +291,6 @@ QString XBinary::convertPathName(QString sPathName)
         sResult.replace("/",QDir::separator());
     }
 
-    if(sPathName.contains("$data"))
-    {
-#ifdef Q_OS_MAC
-        sResult.replace("$data",QCoreApplication::applicationDirPath()+"/../Resources");
-#else
-        sResult.replace("$data",QCoreApplication::applicationDirPath());
-#endif
-        sResult.replace("/",QDir::separator());
-    }
-
     return sResult;
 }
 
