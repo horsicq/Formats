f8eb0c1f748018fef0c44926d765707048a084be
horsicq <horsicq@gmail.com>
2020-08-30 11:23:47 +0200

    Fix

--- xbinary.cpp
+++ xbinary.cpp
@@ -2182,9 +2182,9 @@ XBinary::_MEMORY_RECORD XBinary::getMemoryRecordByOffset(XBinary::_MEMORY_MAP *p
 {
     _MEMORY_RECORD result={};
 
-    int nCount=pMemoryMap->listRecords.count();
+    int nNumberOfRecords=pMemoryMap->listRecords.count();
 
-    for(int i=0; i<nCount; i++)
+    for(int i=0; i<nNumberOfRecords; i++)
     {
         if(pMemoryMap->listRecords.at(i).nSize&&(pMemoryMap->listRecords.at(i).nOffset!=-1))
         {
