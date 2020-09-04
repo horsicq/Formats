d328570e3bbbd408c83e0dc05d5fc95312b407fa
horsicq <horsicq@gmail.com>
2020-09-05 00:20:31 +0200

    Fix

--- xmach.cpp
+++ xmach.cpp
@@ -1050,9 +1050,9 @@ quint32 XMACH::getNumberOfSections(QList<XMACH::COMMAND_RECORD> *pList)
     {
         QList<COMMAND_RECORD> listLCSegments=getCommandRecords(XMACH_DEF::S_LC_SEGMENT_64,pList);
 
-        int nCount=listLCSegments.count();
+        int nNumberOfSegments=listLCSegments.count();
 
-        for(int i=0;i<nCount;i++)
+        for(int i=0;i<nNumberOfSegments;i++)
         {
             qint64 nOffset=listLCSegments.at(i).nOffset;
             int nNumberOfSections=read_uint32(nOffset+offsetof(XMACH_DEF::segment_command_64,nsects),bIsBigEndian);
