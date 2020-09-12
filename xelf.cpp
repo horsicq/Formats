120489573fc69746ac40e809d4dcf7d5e8f37ba5
horsicq <horsicq@gmail.com>
2020-09-12 10:32:32 +0200

    Fix

--- xelf.cpp
+++ xelf.cpp
@@ -2037,13 +2037,13 @@ quint32 XELF::getElf_Shdr_type(quint32 nIndex, QList<XELF_DEF::Elf_Shdr> *pListS
     return nResult;
 }
 
-quint64 XELF::getElf_Shdr_flags(quint32 nIndex, QList<XELF_DEF::Elf_Shdr> *pList)
+quint64 XELF::getElf_Shdr_flags(quint32 nIndex, QList<XELF_DEF::Elf_Shdr> *pListSectionHeaders)
 {
     quint64 nResult=0;
 
-    if(nIndex<(quint32)pList->count())
+    if(nIndex<(quint32)pListSectionHeaders->count())
     {
-        nResult=pList->at(nIndex).sh_flags;
+        nResult=pListSectionHeaders->at(nIndex).sh_flags;
     }
 
     return nResult;
--- xelf.h
+++ xelf.h
@@ -275,7 +275,7 @@ public:
 
     quint32 getElf_Shdr_name(quint32 nIndex,QList<XELF_DEF::Elf_Shdr> *pListSectionHeaders);
     quint32 getElf_Shdr_type(quint32 nIndex,QList<XELF_DEF::Elf_Shdr> *pListSectionHeaders);
-    quint64 getElf_Shdr_flags(quint32 nIndex,QList<XELF_DEF::Elf_Shdr> *pList);
+    quint64 getElf_Shdr_flags(quint32 nIndex,QList<XELF_DEF::Elf_Shdr> *pListSectionHeaders);
     quint64 getElf_Shdr_addr(quint32 nIndex,QList<XELF_DEF::Elf_Shdr> *pList);
     quint64 getElf_Shdr_offset(quint32 nIndex,QList<XELF_DEF::Elf_Shdr> *pList);
     quint64 getElf_Shdr_size(quint32 nIndex,QList<XELF_DEF::Elf_Shdr> *pList);
