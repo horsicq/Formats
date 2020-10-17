459e49604595f173f715f6d805fc3fe7f5287e42
horsicq <horsicq@gmail.com>
2020-10-17 16:33:03 +0200

    Fix

--- xelf.h
+++ xelf.h
@@ -112,6 +112,7 @@ public:
     void setIdent_pad(quint8 nValue,int nPad);
 
     // TODO Hdr32 getHdr32();
+    // TODO Hdr64 getHdr64();
     quint16 getHdr32_type();
     void setHdr32_type(quint16 nValue);
     quint16 getHdr32_machine();
