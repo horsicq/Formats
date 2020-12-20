8f9c4bc7ef04809dfb1c7de0c8aa053327b6776d
horsicq <horsicq@gmail.com>
2020-12-20 12:02:52 +0100

    Fix

--- xformats.cpp
+++ xformats.cpp
@@ -69,7 +69,7 @@ XBinary::_MEMORY_MAP XFormats::getMemoryMap(XBinary::FT fileType,QIODevice *pDev
         XMACH mach(pDevice,bIsImage,nImageBase);
         result=mach.getMemoryMap();
     }
-#ifdef USE_DEX
+#ifdef USE_DEX // TODO Check !!!
     else if(XBinary::checkFileType(XBinary::FT_DEX,fileType))
     {
         XDEX dex(pDevice);
