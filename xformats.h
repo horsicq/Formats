892711408b4082969a71e1c760b6c48b044ce1cc
horsicq <horsicq@gmail.com>
2021-02-19 01:08:58 +0100

    Fix

--- xformats.h
+++ xformats.h
@@ -49,7 +49,7 @@ public:
     static XBinary::_MEMORY_MAP getMemoryMap(XBinary::FT fileType,QIODevice *pDevice,bool bIsImage=false,qint64 nImageBase=0);
     static qint64 getEntryPointAddress(XBinary::FT fileType,QIODevice *pDevice,bool bIsImage=false,qint64 nImageBase=0);
     static qint64 getEntryPointOffset(XBinary::FT fileType,QIODevice *pDevice,bool bIsImage=false,qint64 nImageBase=0);
-    static bool isBigEndian(XBinary::FT fileType,QIODevice *pDevice,bool bIsImage=false,qint64 nImageBase=0);
+    static bool isBigEndian(XBinary::FT fileType,QIODevice *pDevice,bool bIsImage=false,qint64 nImageBase=0); // TODO rename
 #ifdef QT_GUI_LIB
     static XBinary::FT setFileTypeComboBox(QComboBox *pComboBox,QList<XBinary::FT> *pListFileTypes,XBinary::FT fileType);
     static bool setEndianessComboBox(QComboBox *pComboBox,bool bIsBigEndian);
