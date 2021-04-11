a4559d5aca77f361b3a19c4532ec4657efb2b01f
horsicq <horsicq@gmail.com>
2021-04-11 02:19:44 +0200

    Fix

--- xmach_def.h
+++ xmach_def.h
@@ -289,6 +289,59 @@ struct dylib_command
     quint32 cmdsize;
     dylib _dylib;
 };
+
+enum reloc_type_x86_64
+{
+    X86_64_RELOC_UNSIGNED,		// for absolute addresses
+    X86_64_RELOC_SIGNED,		// for signed 32-bit displacement
+    X86_64_RELOC_BRANCH,		// a CALL/JMP instruction with 32-bit displacement
+    X86_64_RELOC_GOT_LOAD,		// a MOVQ load of a GOT entry
+    X86_64_RELOC_GOT,			// other GOT references
+    X86_64_RELOC_SUBTRACTOR,	// must be followed by a X86_64_RELOC_UNSIGNED
+    X86_64_RELOC_SIGNED_1,		// for signed 32-bit displacement with a -1 addend
+    X86_64_RELOC_SIGNED_2,		// for signed 32-bit displacement with a -2 addend
+    X86_64_RELOC_SIGNED_4,		// for signed 32-bit displacement with a -4 addend
+};
+
+enum reloc_type_arm
+{
+    ARM_RELOC_VANILLA,	/* generic relocation as discribed above */
+    ARM_RELOC_PAIR,	/* the second relocation entry of a pair */
+    ARM_RELOC_SECTDIFF,	/* a PAIR follows with subtract symbol value */
+    ARM_RELOC_LOCAL_SECTDIFF, /* like ARM_RELOC_SECTDIFF, but the symbol referenced was local.  */
+    ARM_RELOC_PB_LA_PTR,/* prebound lazy pointer */
+    ARM_RELOC_BR24,	/* 24 bit branch displacement (to a word address) */
+    ARM_THUMB_RELOC_BR22, /* 22 bit branch displacement (to a half-word address) */
+};
+
+enum reloc_type_ppc
+{
+    PPC_RELOC_VANILLA,	/* generic relocation as discribed above */
+    PPC_RELOC_PAIR,	/* the second relocation entry of a pair */
+    PPC_RELOC_BR14,	/* 14 bit branch displacement (to a word address) */
+    PPC_RELOC_BR24,	/* 24 bit branch displacement (to a word address) */
+    PPC_RELOC_HI16,	/* a PAIR follows with the low half */
+    PPC_RELOC_LO16,	/* a PAIR follows with the high half */
+    PPC_RELOC_HA16,	/* Same as the RELOC_HI16 except the low 16 bits and the
+             * high 16 bits are added together with the low 16 bits
+             * sign extened first.  This means if bit 15 of the low
+             * 16 bits is set the high 16 bits stored in the
+             * instruction will be adjusted.
+             */
+    PPC_RELOC_LO14,	/* Same as the LO16 except that the low 2 bits are not
+             * stored in the instruction and are always zero.  This
+             * is used in double word load/store instructions.
+             */
+    PPC_RELOC_SECTDIFF,	/* a PAIR follows with subtract symbol value */
+    PPC_RELOC_PB_LA_PTR,/* prebound lazy pointer */
+    PPC_RELOC_HI16_SECTDIFF, /* section difference forms of above.  a PAIR */
+    PPC_RELOC_LO16_SECTDIFF, /* follows these with subtract symbol value */
+    PPC_RELOC_HA16_SECTDIFF,
+    PPC_RELOC_JBSR,
+    PPC_RELOC_LO14_SECTDIFF,
+    PPC_RELOC_LOCAL_SECTDIFF  /* like PPC_RELOC_SECTDIFF, but the symbol
+                 referenced was local.  */
+};
 }
 
 #endif // QMACH_DEF_H
