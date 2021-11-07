f35600cd00e7a7e4b889943d07d7718ebd8e5434
horsicq <horsicq@gmail.com>
2021-11-07 20:53:16 +0100

    Fix: 2021-11-07

--- xbinary.cpp
+++ xbinary.cpp
@@ -3280,9 +3280,9 @@ qint64 XBinary::addressToOffset(XBinary::_MEMORY_MAP *pMemoryMap, qint64 nAddres
         }
     }
 
-    int nNumberOfRecords=pMemoryMap->listRecords.count();
+    qint32 nNumberOfRecords=pMemoryMap->listRecords.count();
 
-    for(int i=0;i<nNumberOfRecords;i++)
+    for(qint32 i=0;i<nNumberOfRecords;i++)
     {
         if(pMemoryMap->listRecords.at(i).nSize&&(pMemoryMap->listRecords.at(i).nAddress!=-1)&&(pMemoryMap->listRecords.at(i).nOffset!=-1))
         {
