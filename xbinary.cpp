e8d4c9f9197c0a9ea7fb775866ca7ba4e256043c
horsicq <horsicq@gmail.com>
2021-11-10 21:45:08 +0100

    Fix: 2021-11-10

--- xbinary.cpp
+++ xbinary.cpp
@@ -7783,7 +7783,7 @@ XBinary::SYMBOL_RECORD XBinary::findSymbolByName(QList<SYMBOL_RECORD> *pListSymb
 
     qint32 nNumberOfRecords=pListSymbolRecords->count();
 
-    for(int i=0;i<nNumberOfRecords;i++)
+    for(qint32 i=0;i<nNumberOfRecords;i++)
     {
         if(pListSymbolRecords->at(i).sName==sName)
         {
