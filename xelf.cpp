587c7b5058fb6de156a933dcde57edcee2151928
horsicq <horsicq@gmail.com>
2021-12-08 03:50:19 +0100

    Fix: 2021-12-08

--- xelf.cpp
+++ xelf.cpp
@@ -3820,10 +3820,10 @@ QList<XELF::SECTION_RECORD> XELF::getSectionRecords(QList<XELF_DEF::Elf_Shdr> *p
 {
     QList<SECTION_RECORD> listRecords;
 
-    int nNumberOfSections=pListSectionHeaders->count();
+    qint32 nNumberOfSections=pListSectionHeaders->count();
     quint32 nSectionTableSize=pbaSectionTable->size();
 
-    for(int i=0;i<nNumberOfSections;i++)
+    for(qint32 i=0;i<nNumberOfSections;i++)
     {
         SECTION_RECORD record={};
 
