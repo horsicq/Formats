def740fb42cbb7383563c757c3353507ceac0a04
horsicq <horsicq@gmail.com>
2022-02-25 00:57:30 +0100

    Fix: 2022-02-25

--- subdevice.cpp
+++ subdevice.cpp
@@ -20,7 +20,7 @@
  */
 #include "subdevice.h"
 
-SubDevice::SubDevice(QIODevice *pDevice, qint64 nOffset, qint64 nSize, QObject *pParent) : QIODevice(pParent)
+SubDevice::SubDevice(QIODevice *pDevice,qint64 nOffset,qint64 nSize,QObject *pParent) : QIODevice(pParent)
 {
     if(nOffset>pDevice->size())
     {
