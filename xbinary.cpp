1a6486539b8d0525e0622edbccaa7bb929f6639c
horsicq <horsicq@gmail.com>
2022-02-25 08:28:33 +0100

    Fix: 2022-02-25

--- xbinary.cpp
+++ xbinary.cpp
@@ -608,7 +608,7 @@ void XBinary::findFiles(QString sDirectoryName, QList<QString> *pListFileNames)
     }
 }
 
-QString XBinary::regExp(QString sRegExp, QString sString, qint32 nIndex)
+QString XBinary::regExp(QString sRegExp,QString sString,qint32 nIndex)
 {
     QString sResult;
 #if (QT_VERSION_MAJOR<5)
