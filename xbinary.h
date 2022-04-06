ef37bfb37d691c6420d27f204a7c265819693528
horsicq <horsicq@gmail.com>
2022-04-06 18:06:22 +0200

    Fix: 2022-04-06

--- xbinary.h
+++ xbinary.h
@@ -206,6 +206,92 @@ public:
     #endif
     };
 
+    struct REG_OPTIONS
+    {
+        bool bGeneral;
+        bool bIP;
+        bool bFlags;
+        bool bSegments;
+        bool bDebug;
+        bool bFloat;
+        bool bXMM;
+    };
+
+    struct REGISTERS
+    {
+        bool bIsValid;
+    #ifdef Q_PROCESSOR_X86_32
+        quint32 EAX;
+        quint32 ECX;
+        quint32 EDX;
+        quint32 EBX;
+        quint32 ESP;
+        quint32 EBP;
+        quint32 ESI;
+        quint32 EDI;
+        quint32 EIP;
+    #endif
+    #ifdef Q_PROCESSOR_X86_64
+        quint64 RAX;
+        quint64 RCX;
+        quint64 RDX;
+        quint64 RBX;
+        quint64 RSP;
+        quint64 RBP;
+        quint64 RSI;
+        quint64 RDI;
+        quint64 R8;
+        quint64 R9;
+        quint64 R10;
+        quint64 R11;
+        quint64 R12;
+        quint64 R13;
+        quint64 R14;
+        quint64 R15;
+        quint64 RIP;
+    #endif
+    #ifdef Q_PROCESSOR_X86
+        quint32 EFLAGS;
+        quint16 CS;
+        quint16 DS;
+        quint16 ES;
+        quint16 FS;
+        quint16 GS;
+        quint16 SS;
+    #endif
+    #ifdef Q_PROCESSOR_X86_32
+        quint32 DR[8];
+    #endif
+    #ifdef Q_PROCESSOR_X86_64
+        quint64 DR[8];
+    #endif
+    };
+
+    struct PROCESS_INFO
+    {
+        QString sName;
+        //        qint64 nParentID;
+        qint64 nID;
+        QString sFilePath;
+        quint64 nImageAddress;
+        quint64 nImageSize;
+    };
+
+    struct MODULE
+    {
+        quint64 nAddress;
+        quint64 nSize;
+        QString sName;
+        QString sFileName;
+    };
+
+    struct STATUS
+    {
+        REGISTERS registers;
+        QList<MEMORY_REGION> listMemoryRegions;
+        QList<MODULE> listModules;
+    };
+
     enum FORMATTYPE
     {
         FORMATTYPE_TEXT=0,
