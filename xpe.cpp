fd90f9918fcba42f7e1e5082c3406710e6890b80
horsicq <horsicq@gmail.com>
2022-04-16 22:05:53 +0200

    Fix: 2022-04-16

--- xpe.cpp
+++ xpe.cpp
@@ -3259,7 +3259,7 @@ QList<XPE::RESOURCE_RECORD> XPE::getResources(XBinary::_MEMORY_MAP *pMemoryMap)
 
                         if(rd[2].NumberOfIdEntries+rd[2].NumberOfNamedEntries<=1000)
                         {
-                            for(int k=0; k<rd[2].NumberOfIdEntries+rd[2].NumberOfNamedEntries; k++)
+                            for(int k=0;k<rd[2].NumberOfIdEntries+rd[2].NumberOfNamedEntries;k++)
                             {
                                 rde[2]=read_IMAGE_RESOURCE_DIRECTORY_ENTRY(nOffsetLevel[2]);
 
