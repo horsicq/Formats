178c2fa57d70632eaae781012f21531d22a04be9
horsicq <horsicq@gmail.com>
2022-07-04 18:33:36 +0200

    Fix: 2022-07-04

--- xbinary.cpp
+++ xbinary.cpp
@@ -1648,7 +1648,7 @@ qint64 XBinary::find_int8(qint64 nOffset, qint64 nSize, qint8 nValue, PDSTRUCT *
     return find_array(nOffset,nSize,(char *)&nValue,1,pProcessData);
 }
 
-qint64 XBinary::find_uint16(qint64 nOffset, qint64 nSize, quint16 nValue, bool bIsBigEndian, PDSTRUCT *pProcessData)
+qint64 XBinary::find_uint16(qint64 nOffset,qint64 nSize,quint16 nValue,bool bIsBigEndian,PDSTRUCT *pProcessData)
 {
     if(bIsBigEndian)
     {
