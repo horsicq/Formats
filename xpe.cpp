4f1777540de1de290e62406a5849129bf569e7e0
horsicq <horsicq@gmail.com>
2022-09-01 18:41:25 +0200

    Fix: 2022-09-01

--- xpe.cpp
+++ xpe.cpp
@@ -20,7 +20,7 @@
  */
 #include "xpe.h"
 
-XPE::XPE(QIODevice *pDevice, bool bIsImage, XADDR nModuleAddress): XMSDOS(pDevice,bIsImage,nModuleAddress)
+XPE::XPE(QIODevice *pDevice,bool bIsImage,XADDR nModuleAddress): XMSDOS(pDevice,bIsImage,nModuleAddress)
 {
 }
 
