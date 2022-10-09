8a93905e6785e99798d6fb5249379d94fd52661a
horsicq <horsicq@gmail.com>
2022-10-09 17:51:39 +0200

    Fix: 2022-10-09

--- xelf.cpp
+++ xelf.cpp
@@ -2018,7 +2018,7 @@ void XELF::setElf64_Shdr_flags(quint32 nIndex,quint64 nValue)
     }
 }
 
-void XELF::setElf64_Shdr_addr(quint32 nIndex, quint64 nValue)
+void XELF::setElf64_Shdr_addr(quint32 nIndex,quint64 nValue)
 {
     quint32 nNumberOfSections=getHdr64_shnum();
     quint64 offset=getHdr64_shoff();
