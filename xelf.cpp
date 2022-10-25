44ae4a867957c67877d7257fffffc9cd781ae941
horsicq <horsicq@gmail.com>
2022-10-25 15:38:44 +0200

    Fix: 2022-10-25

--- xelf.cpp
+++ xelf.cpp
@@ -2145,7 +2145,7 @@ quint64 XELF::getElf_Shdr_flags(quint32 nIndex,QList<XELF_DEF::Elf_Shdr> *pListS
     return nResult;
 }
 
-quint64 XELF::getElf_Shdr_addr(quint32 nIndex, QList<XELF_DEF::Elf_Shdr> *pListSectionHeaders)
+quint64 XELF::getElf_Shdr_addr(quint32 nIndex,QList<XELF_DEF::Elf_Shdr> *pListSectionHeaders)
 {
     quint64 nResult=0;
 
