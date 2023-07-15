fb12247584fb5d3aea5dfd3b9d1761f2953e0f03
horsicq <horsicq@gmail.com>
2023-07-16 00:31:17 +0200

    Fix: 2023-07-16

--- xformats.cpp
+++ xformats.cpp
@@ -863,8 +863,8 @@ Qt::GlobalColor XFormats::typeToColor(const QString &sType)
     // TODO more
     if ((_sType == "installer") || (_sType == "sfx")) {
         result = Qt::blue;
-    } else if ((_sType == "protector") || (_sType == "apk obfuscator") || (_sType == "jar obfuscator") || (_sType == ".net obfuscator") || (_sType == ".net compressor") ||
-               (_sType == "dongle protection") || (_sType == "joiner") || (_sType == "packer")) {
+    } else if ((_sType == "protector") || (_sType == "apk obfuscator") || (_sType == "jar obfuscator") || (_sType == ".net obfuscator") ||
+               (_sType == ".net compressor") || (_sType == "dongle protection") || (_sType == "joiner") || (_sType == "packer")) {
         result = Qt::red;
     } else if ((_sType == "pe tool") || (_sType == "apk tool")) {
         result = Qt::green;
