fb088b205e32009264ea8899bc3e9adbeee0f0ca
horsicq <horsicq@gmail.com>
2024-09-30 00:09:05 +0200

    Update file(s): 2024-09-30

--- images/xjpeg.cpp
+++ images/xjpeg.cpp
@@ -52,9 +52,10 @@ XBinary::_MEMORY_MAP XJpeg::getMemoryMap(MAPMODE mapMode, PDSTRUCT *pPdStruct)
 {
     Q_UNUSED(mapMode)
 
-    PDSTRUCT pdStructEmpty = XBinary::createPdStruct();
+    XBinary::PDSTRUCT pdStructEmpty = {};
 
     if (!pPdStruct) {
+        pdStructEmpty = XBinary::createPdStruct();
         pPdStruct = &pdStructEmpty;
     }
 
