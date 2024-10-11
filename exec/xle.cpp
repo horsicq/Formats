b2d13ed08cc98799a1ed0cf87321c19221aee9ba
horsicq <horsicq@gmail.com>
2024-10-12 01:32:55 +0200

    Update file(s): 2024-10-12

--- exec/xle.cpp
+++ exec/xle.cpp
@@ -1242,9 +1242,10 @@ XBinary::_MEMORY_MAP XLE::getMemoryMap(MAPMODE mapMode, PDSTRUCT *pPdStruct)
 {
     Q_UNUSED(mapMode)
 
-    PDSTRUCT pdStructEmpty = XBinary::createPdStruct();
+    XBinary::PDSTRUCT pdStructEmpty = {};
 
     if (!pPdStruct) {
+        pdStructEmpty = XBinary::createPdStruct();
         pPdStruct = &pdStructEmpty;
     }
 
