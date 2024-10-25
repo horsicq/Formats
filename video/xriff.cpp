0d5d71ac06be1360ba84c6d3d15af1807fa0c5e1
horsicq <horsicq@gmail.com>
2024-10-25 23:38:56 +0200

    Update file(s): 2024-10-25

--- video/xriff.cpp
+++ video/xriff.cpp
@@ -90,9 +90,10 @@ XBinary::_MEMORY_MAP XRiff::getMemoryMap(MAPMODE mapMode, PDSTRUCT *pPdStruct)
 {
     Q_UNUSED(mapMode)
 
-    PDSTRUCT pdStructEmpty = XBinary::createPdStruct();
+    XBinary::PDSTRUCT pdStructEmpty = {};
 
     if (!pPdStruct) {
+        pdStructEmpty = XBinary::createPdStruct();
         pPdStruct = &pdStructEmpty;
     }
 
