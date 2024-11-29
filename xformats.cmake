3b5473e7defcea7e32737321af543ed3b947966f
horsicq <horsicq@gmail.com>
2024-11-29 07:49:30 +0100

    Update file(s): 2024-11-29

--- xformats.cmake
+++ xformats.cmake
@@ -4,12 +4,17 @@ include_directories(${CMAKE_CURRENT_LIST_DIR}/video)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/audio)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/exec)
 
-include(${CMAKE_CURRENT_LIST_DIR}/../XDEX/xdex.cmake)
+if (NOT DEFINED XDEX_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../XDEX/xdex.cmake)
+    set(XFORMATS_SOURCES ${XFORMATS_SOURCES} ${XDEX_SOURCES})
+endif()
+
 include(${CMAKE_CURRENT_LIST_DIR}/../XPDF/xpdf.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../XArchive/xarchives.cmake)
 # TODO
 
 set(XFORMATS_SOURCES
+    ${XFORMATS_SOURCES}
     ${XDEX_SOURCES}
     ${XPDF_SOURCES}
     ${XARCHIVES_SOURCES}
