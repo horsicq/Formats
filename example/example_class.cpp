59e8c2f597e27b9937d178039842bc96dd223757
horsicq <horsicq@gmail.com>
2025-06-04 19:20:11 +0200

    Add new file(s): 2025-06-04

---
+++ example/example_class.cpp
@@ -0,0 +1,213 @@
+/* Copyright (c) 2025 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "example_class.h"
+
+EXAMPLE_CLASS::EXAMPLE_CLASS(QIODevice *pDevice = nullptr) : XBinary(pDevice)
+{
+    // Constructor implementation
+}
+
+EXAMPLE_CLASS::~EXAMPLE_CLASS()
+{
+    // Destructor implementation
+}
+
+bool EXAMPLE_CLASS::isValid(PDSTRUCT *pPdStruct)
+{
+    // Check if the file is valid
+    bool bResult = false;
+
+    if (getSize() > 0) {
+        // Perform validation checks here
+        bResult = true;  // Replace with actual validation logic
+    }
+
+    return bResult;
+}
+
+XBinary::FT EXAMPLE_CLASS::getFileType()
+{
+    // Return the file type
+    return XBinary::FT_UNKNOWN;  // Replace with actual implementation
+}
+
+XBinary::MODE EXAMPLE_CLASS::getMode()
+{
+    // Return the mode
+    return XBinary::MODE_UNKNOWN;  // Replace with actual implementation
+}
+
+QString EXAMPLE_CLASS::getMIMEString()
+{
+    // Return the MIME type string
+    return QString();  // Replace with actual implementation
+}
+
+qint32 EXAMPLE_CLASS::getType()
+{
+    // Return the type
+    return 0;  // Replace with actual implementation
+}
+
+QString EXAMPLE_CLASS::typeIdToString(qint32 nType)
+{
+    // Convert type ID to string
+    return QString::number(nType);  // Replace with actual implementation
+}
+
+XBinary::ENDIAN EXAMPLE_CLASS::getEndian()
+{
+    // Return the endianness
+    return XBinary::ENDIAN_UNKNOWN;  // Replace with actual implementation
+}
+
+QString EXAMPLE_CLASS::getArch()
+{
+    // Return the architecture string
+    return QString();  // Replace with actual implementation
+}
+
+QString EXAMPLE_CLASS::getFileFormatExt()
+{
+    // Return the file format extension
+    return QString();  // Replace with actual implementation
+}
+
+qint64 EXAMPLE_CLASS::getFileFormatSize(XBinary::PDSTRUCT *pPdStruct)
+{
+    // Return the file format size
+    return -1;  // Replace with actual implementation
+}
+
+bool EXAMPLE_CLASS::isSigned()
+{
+    // Check if the file is signed
+    return false;  // Replace with actual implementation
+}
+
+QString EXAMPLE_CLASS::getOsVersion()
+{
+    // Return the OS version string
+    return QString();  // Replace with actual implementation
+}
+
+QString EXAMPLE_CLASS::getVersion()
+{
+    // Return the version string
+    return QString();  // Replace with actual implementation
+}
+
+QString EXAMPLE_CLASS::getInfo()
+{
+    // Return additional information about the file
+    return QString();  // Replace with actual implementation
+}
+
+bool EXAMPLE_CLASS::isEncrypted()
+{
+    // Check if the file is encrypted
+    return false;  // Replace with actual implementation
+}
+
+QList<QString> EXAMPLE_CLASS::getTableTitles(const DATA_RECORDS_OPTIONS &dataRecordsOptions)
+{
+    // Return a list of table titles based on the provided options
+    QList<QString> listTitles;
+
+    // Populate the list with titles as needed
+    // ...
+
+    return listTitles;  // Replace with actual implementation
+}
+
+qint32 EXAMPLE_CLASS::readTableRow(qint32 nRow, LT locType, XADDR nLocation, const DATA_RECORDS_OPTIONS &dataRecordsOptions, QList<QVariant> *pListValues, PDSTRUCT *pPdStruct)
+{
+    // Read a row from the table based on the provided parameters
+    qint32 nResult = 0;
+
+    // Perform reading logic here
+    // ...
+
+    return nResult;  // Replace with actual implementation
+}
+
+QList<XBinary::DATA_HEADER> EXAMPLE_CLASS::getDataHeaders(const DATA_HEADERS_OPTIONS &dataHeadersOptions, PDSTRUCT *pPdStruct)
+{
+    // Return a list of data headers based on the provided options
+    QList<DATA_HEADER> listDataHeaders;
+
+    // Populate the list with data headers as needed
+    // ...
+
+    return listDataHeaders;  // Replace with actual implementation
+}
+
+QList<XBinary::HREGION> EXAMPLE_CLASS::getHData(PDSTRUCT *pPdStruct)
+{
+    // Return a list of HData regions
+    QList<HREGION> listHData;
+    // Populate the list with HData as needed
+    // ...
+
+    return listHData;  // Replace with actual implementation
+}
+
+QList<XBinary::HREGION> EXAMPLE_CLASS::getNativeSubRegions(PDSTRUCT *pPdStruct)
+{
+    // Return a list of native sub-regions
+    QList<HREGION> listSubRegions;
+    // Populate the list with sub-regions as needed
+    // ...
+
+    return listSubRegions;  // Replace with actual implementation
+}
+
+QList<XBinary::HREGION> EXAMPLE_CLASS::getNativeRegions(PDSTRUCT *pPdStruct)
+{
+    // Return a list of native regions
+    QList<HREGION> listRegions;
+    // Populate the list with regions as needed
+    // ...
+
+    return listRegions;  // Replace with actual implementation
+}
+
+XBinary::_MEMORY_MAP EXAMPLE_CLASS::getMemoryMap(MAPMODE mapMode, PDSTRUCT *pPdStruct)
+{
+    // Return the memory map based on the specified mode
+    _MEMORY_MAP memoryMap = {};
+    memoryMap.fileType = getFileType();
+    memoryMap.mode = getMode();
+    memoryMap.sArch = getArch();
+    memoryMap.endian = getEndian();
+    memoryMap.sType = typeIdToString(getType());
+
+    // Populate other fields as needed
+    // ...
+
+    return memoryMap;  // Replace with actual implementation
+}
+
+XBinary::OSNAME EXAMPLE_CLASS::getOsName()
+{
+    // Return the OS name
+    return XBinary::OSNAME_UNKNOWN;  // Replace with actual implementation
+}
---
+++ example/example_class.h
@@ -0,0 +1,62 @@
+/* Copyright (c) 2025 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef EXAMPLE_CLASS_H
+#define EXAMPLE_CLASS_H
+
+#include <QObject>
+#include <xbinary.h>
+
+class EXAMPLE_CLASS : public XBinary
+{
+    Q_OBJECT
+public:
+    EXAMPLE_CLASS(QIODevice *pDevice = nullptr);
+    ~EXAMPLE_CLASS();
+
+    virtual bool isValid(PDSTRUCT *pPdStruct = nullptr);
+    virtual FT getFileType();
+    virtual MODE getMode();
+    virtual QString getMIMEString();
+    virtual qint32 getType();
+    virtual QString typeIdToString(qint32 nType);
+    virtual ENDIAN getEndian();
+    virtual QString getArch();
+    virtual QString getFileFormatExt();
+    virtual qint64 getFileFormatSize(PDSTRUCT *pPdStruct);
+    virtual bool isSigned();
+    virtual OSNAME getOsName();
+    virtual QString getOsVersion();
+    virtual QString getVersion();
+    virtual QString getInfo();
+    virtual bool isEncrypted();
+    virtual QList<MAPMODE> getMapModesList();
+    virtual _MEMORY_MAP getMemoryMap(MAPMODE mapMode = MAPMODE_UNKNOWN, PDSTRUCT *pPdStruct = nullptr);
+
+    virtual QList<HREGION> getNativeRegions(PDSTRUCT *pPdStruct = nullptr);
+    virtual QList<HREGION> getNativeSubRegions(PDSTRUCT *pPdStruct = nullptr);
+    virtual QList<HREGION> getHData(PDSTRUCT *pPdStruct = nullptr);
+
+    virtual QList<DATA_HEADER> getDataHeaders(const DATA_HEADERS_OPTIONS &dataHeadersOptions, PDSTRUCT *pPdStruct);
+    virtual QList<QString> getTableTitles(const DATA_RECORDS_OPTIONS &dataRecordsOptions);
+    virtual qint32 readTableRow(qint32 nRow, LT locType, XADDR nLocation, const DATA_RECORDS_OPTIONS &dataRecordsOptions, QList<QVariant> *pListValues, PDSTRUCT *pPdStruct);
+};
+
+#endif // EXAMPLE_CLASS_H
