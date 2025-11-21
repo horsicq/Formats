2d6840406eedafcb16c6ecac0a152c29e5ce2345
horsicq <horsicq@gmail.com>
2025-11-21 18:25:12 +0100

    Update file(s): 2025-11-21

--- xformats.cmake
+++ xformats.cmake
@@ -91,12 +91,8 @@ set(XFORMATS_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/exec/xpe_def.h
     ${CMAKE_CURRENT_LIST_DIR}/formats/xjavaclass.h
     ${CMAKE_CURRENT_LIST_DIR}/formats/xjavaclass.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/formats/xpyc.h
-    ${CMAKE_CURRENT_LIST_DIR}/formats/xpyc.cpp
     ${CMAKE_CURRENT_LIST_DIR}/formats/xttf.h
     ${CMAKE_CURRENT_LIST_DIR}/formats/xttf.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/formats/xriff.h
-    ${CMAKE_CURRENT_LIST_DIR}/formats/xriff.cpp
     ${CMAKE_CURRENT_LIST_DIR}/formats/xdjvu.h
     ${CMAKE_CURRENT_LIST_DIR}/formats/xdjvu.cpp
     ${CMAKE_CURRENT_LIST_DIR}/texts/xtext.cpp
