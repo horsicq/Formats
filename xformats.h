dd529cf4fac6c19fc6146f6d7f79d0cd1fbdee60
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2026-03-21 05:45:04 +0100

    revert xformats.h
    Revert xformats.h back to state before: 372c05d9c95020e674bb38b5498a9c220c8b8e41

--- xformats.h
+++ xformats.h
@@ -51,7 +51,7 @@
 #include "xtext.h"
 #include "xdjvu.h"
 #include "xder.h"
-#include "xlzip.h"
+#include "../../XArchive/xlzip.h"
 #include "../../XOptions/xthreadobject.h"
 #ifdef USE_DEX
 #include "xandroidbinary.h"
